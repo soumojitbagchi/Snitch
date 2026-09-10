cfcd88a3c699b5c9ec4aaf1ff2118b9d150e6e97
Soumojit Bagchi <soumojitbagchi001@gmail.com>
2026-09-11 02:22:12 +0530

    feat(product): add endpoints to fetch all products and products by seller

--- backend/src/controller/product.controller.js
+++ backend/src/controller/product.controller.js
@@ -2,77 +2,112 @@ import Product from "../model/product.model.js";
 import uploadFiles from "../service/imageKit.service.js";
 
 export const createProduct = async (req, res) => {
-  try {
-    const { title, description, priceAmount, priceCurrency } = req.body;
-    const seller = req.user;
-    const files = await Promise.all(
-      req.files.map((file) => {
-        return uploadFiles({
-          buffer: file.buffer,
-          fileName: file.originalname,
+    try {
+        const { title, description, priceAmount, priceCurrency } = req.body;
+        const seller = req.user;
+        const files = await Promise.all(
+            req.files.map((file) => {
+                return uploadFiles({
+                    buffer: file.buffer,
+                    fileName: file.originalname,
+                });
+            }),
+        );
+        const product = await Product.create({
+            title,
+            description,
+            images: files,
+            price: {
+                amount: priceAmount,
+                currency: priceCurrency || "INR",
+            },
+            seller,
         });
-      }),
-    );
-    const product = await Product.create({
-      title,
-      description,
-      images: files,
-      price: {
-        amount: priceAmount,
-        currency: priceCurrency || "INR",
-      },
-      seller,
-    });
-    res.status(201).json({ message: "Product created successfully", product });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+        res.status(201).json({ message: "Product created successfully", product, success: true });
+    } catch (error) {
+        res.status(500).json({ message: error.message, success: false });
+    }
 };
 export const updatePriceInfo = async (req, res) => {
-  const user = req.user;
-  const { priceAmount } = req.body;
-  const data = await Product.findOne({ _id: req.params.id, seller: user._id });
-  if (!data) {
-    return res.status(404).json({ message: "Product not found" });
-  }
-  data.price.basePrice = priceAmount;
-  await data.save();
-  res.status(200).json({ message: "Price updated successfully", data });
+    try {
+
+        const user = req.user;
+        const { priceAmount } = req.body;
+        const data = await Product.findOne({ _id: req.params.id, seller: user._id });
+        if (!data) {
+            return res.status(404).json({ message: "Product not found", success: false });
+        }
+        data.price.basePrice = priceAmount;
+        await data.save();
+        res.status(200).json({ message: "Price updated successfully", data, success: true });
+    } catch (error) {
+        res.status(500).json({ message: error.message, success: false });
+    }
 };
 export const updateTitle = async (req, res) => {
-  const user = req.user;
-  const { title } = req.body;
-  const data = await Product.findOne({ _id: req.params.id, seller: user._id });
-  if (!data) {
-    return res.status(404).json({ message: "Product not found" });
-  }
-  data.title = title;
-  await data.save();
-  res.status(200).json({ message: "Title updated successfully", data });
+    try {
+        const user = req.user;
+        const { title } = req.body;
+        const data = await Product.findOne({ _id: req.params.id, seller: user._id });
+        if (!data) {
+            return res.status(404).json({ message: "Product not found", success: false });
+        }
+        data.title = title;
+        await data.save();
+        res.status(200).json({ message: "Title updated successfully", data, success: true });
+    } catch (error) {
+        res.status(500).json({ message: error.message, success: false });
+    }
 };
 export const updateDescription = async (req, res) => {
-  const user = req.user;
-  const { description } = req.body;
-  const data = await Product.findOne({ _id: req.params.id, seller: user._id });
-  if (!data) {
-    return res.status(404).json({ message: "Product not found" });
-  }
-  data.description = description;
-  await data.save();
-  res.status(200).json({ message: "Description updated successfully", data });
+    try {
+        const user = req.user;
+        const { description } = req.body;
+        const data = await Product.findOne({ _id: req.params.id, seller: user._id });
+        if (!data) {
+            return res.status(404).json({ message: "Product not found", success: false });
+        }
+        data.description = description;
+        await data.save();
+        res.status(200).json({ message: "Description updated successfully", data, success: true });
+    } catch (error) {
+        res.status(500).json({ message: error.message, success: false });
+    }
 };
 export const updateProductImage = async (req, res) => {
-  const user = req.user;
-  const files = await Promise.all(
-    req.files.map((file) =>{
-        return uploadFiles({ buffer: file.buffer, fileName: file.originalname })
-    }),
-  );
-  const data = await Product.findOne({ _id: req.params.id, seller: user._id });
-  if (!data) {
-    return res.status(404).json({ message: "Product not found" });
-  }
-  data.images=files
-  await data.save();
-  res.status(200).json({ message: "Description updated successfully", data });
+    try {
+
+        const user = req.user;
+        const files = await Promise.all(
+            req.files.map((file) => {
+                return uploadFiles({ buffer: file.buffer, fileName: file.originalname })
+            }),
+        );
+        const data = await Product.findOne({ _id: req.params.id, seller: user._id });
+        if (!data) {
+            return res.status(404).json({ message: "Product not found", success: false });
+        }
+        data.images = files
+        await data.save();
+        res.status(200).json({ message: "Description updated successfully", data, success: true });
+    } catch (error) {
+        res.status(500).json({ message: error.message, success: false });
+    }
+};
+export const allProducts = async (req, res) => {
+    try {
+        const data = await Product.find();
+        res.status(200).json({ message: "Products fetched successfully", data, success: true });
+    } catch (error) {
+        res.status(500).json({ message: error.message, success: false });
+    }
+};
+export const allProductsBySeller = async (req, res) => {
+    try {
+        const user = req.user;
+        const data = await Product.find({ seller: user._id });
+        res.status(200).json({ message: "Products fetched successfully", data, success: true });
+    } catch (error) {
+        res.status(500).json({ message: error.message, success: false });
+    }
 };
--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createProduct , updateProductImage, updatePriceInfo, updateTitle, updateDescription } from "../controller/product.controller.js";
+import { createProduct , updateProductImage, updatePriceInfo, updateTitle, updateDescription, allProducts, allProductsBySeller } from "../controller/product.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 import multer from 'multer'
 
@@ -16,5 +16,7 @@ productRouter.put("/update-image/:id", authMiddleware, upload.array('images',5),
 productRouter.put("/update-price/:id", authMiddleware, updatePriceInfo);
 productRouter.put("/update-title/:id", authMiddleware, updateTitle);
 productRouter.put("/update-description/:id", authMiddleware, updateDescription);
+productRouter.get("/all", authMiddleware, allProducts);
+productRouter.get("/all-by-seller", authMiddleware, allProductsBySeller);
 
 export default productRouter;
